ef97588fd284dd47e98367949ea52c22e1289bb2
pavanreddy <pavanreddy5658@gmail.com>
2023-04-06 20:29:45 +0530

    finally decimal to binary logic created

--- DLD_app.py
+++ DLD_app.py
@@ -1,16 +1,33 @@
 class deci_binary_conversion:
     def __init__(self, a):
-        self.r = ''
+        self.r1 = ''
+        self.r2 = ''
         self.bin_ = int(a)
         self.bin_dec = a - self.bin_
+        self.bn = self.binary_no(self.bin_)
+        self.bd = self.binary_dec(self.bin_dec)
+        self.result = self.binary_result(self.bn, self.bd)
 
-    def binary_no(self, bin):
-        self.r += str(bin % 2)
-        if bin // 2 == 0:
-            return int(self.r[::-1])
+    def binary_result(self, bn, bd):
+        return "{:.4f}".format(float(bn + '.' + bd))
+
+    def binary_no(self, bin_):
+        self.r1 += str(bin_ % 2)
+        if bin_ // 2 == 0:
+            return self.r1[::-1]
         else:
-            return self.binary_no(bin // 2)
+            return self.binary_no(bin_ // 2)
 
-a = int(input())
+    def binary_dec(self, bin_dec):
+        while bin_dec-int(bin_dec)!= 0:
+            bin_dec *= 2
+            if bin_dec < 1:
+                self.r2 += '0'   
+            else:
+                bin_dec -= 1
+                self.r2 += '1'
+        return self.r2
+a = float(input())
 c = deci_binary_conversion(a)
-print(c.binary_no(c.bin_))
+print(c.result)
+
